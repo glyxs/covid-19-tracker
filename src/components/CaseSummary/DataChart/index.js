9b57c66849bd39841f0d20ddf7f8de3d5fccd363
Stepan Pavlov <pvlv.stepan@gmail.com>
2021-04-21 18:51:42 +0900

    Fixed "Transparent black"
    DataChart in safari had incorrect gradient color.

--- src/components/CaseSummary/DataChart/index.js
+++ src/components/CaseSummary/DataChart/index.js
@@ -1,36 +1,37 @@
-import { Skeleton, Box, useColorModeValue } from '@chakra-ui/react';
-import { Line } from 'react-chartjs-2';
-import { options } from './options';
-import React from 'react';
+import { Skeleton, Box, useColorModeValue } from "@chakra-ui/react";
+import { Line } from "react-chartjs-2";
+import { options } from "./options";
+import React from "react";
 
 const DataChart = ({ data, isLoading }) => {
+  const bg = useColorModeValue("white", "gray.800");
+  const gradientBg = useColorModeValue(
+    "rgba(255, 255, 255, 0)",
+    "rgba(51, 51, 51, 0)"
+  );
+  const gradient = "linear(to-t, " + bg + ", " + gradientBg + ")";
 
-    const bg = useColorModeValue("white", "gray.800");
-    const gradient = "linear(to-t, " + bg + ", rgba(0, 0, 0, 0))";
-
-    return (
-        <Skeleton
-            pos="relative"
-            zIndex="5"
-            isLoaded={!isLoading}
-        >
-            <Box
-                pos="absolute"
-                zIndex="6"
-                w="100%"
-                h="50px"
-                bottom="0"
-                left="0"
-                pointerEvents="none"
-                bgGradient={gradient}
-            />
-            <Line
-                height={150}
-                data={(data && data) || { labels: [1, 2], datasets: [{ data: [1, 2] }] }}
-                options={options}
-            />
-        </Skeleton>
-    );
+  return (
+    <Skeleton pos="relative" zIndex="5" isLoaded={!isLoading} bg="transparent">
+      <Box
+        pos="absolute"
+        zIndex="6"
+        w="100%"
+        h="50px"
+        bottom="0"
+        left="0"
+        pointerEvents="none"
+        bgGradient={gradient}
+      />
+      <Line
+        height={150}
+        data={
+          (data && data) || { labels: [1, 2], datasets: [{ data: [1, 2] }] }
+        }
+        options={options}
+      />
+    </Skeleton>
+  );
 };
 
 export default DataChart;
