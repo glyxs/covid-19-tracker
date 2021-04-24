5d2191728abe5c4e428f59c1b368067119cd2a98
Stepan Pavlov <pvlv.stepan@gmail.com>
2021-04-25 07:52:41 +0900

    Added spacer to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,6 +4,7 @@ import {
     Link,
     Button,
     useDisclosure,
+    Spacer,
 } from '@chakra-ui/react';
 import React from 'react';
 import PopupForm from './PopupForm';
@@ -16,7 +17,8 @@ const Footer = () => {
         <>
             <Flex mb={6} color="GrayText" fontWeight="600" wrap="wrap" justifyContent="center">
                 <Text >Source: <Link isExternal={true} href="https://www.worldometers.info/coronavirus/">Worldometers</Link>, <Link isExternal={true} href="https://github.com/CSSEGISandData/COVID-19">JHUCSSE</Link></Text>
-                <Button mx={6} onClick={onOpen} variant="link" color="GrayText">Report a bug</Button>
+                <Spacer />
+                <Button onClick={onOpen} variant="link" color="GrayText">Report a bug</Button>
             </Flex>
             <PopupForm onClose={onClose} isOpen={isOpen} />
         </>
