17cd03b6a093ee5ec9f29f56a21691a6b6033baf
Stepan Pavlov <pvlv.stepan@gmail.com>
2021-04-26 13:18:19 +0900

    Added Case Type selector for Country Table

--- src/components/CountryTable/index.js
+++ src/components/CountryTable/index.js
@@ -1,4 +1,4 @@
-import { Box, Flex, Spacer, useColorModeValue, Heading, Skeleton } from '@chakra-ui/react';
+import { Box, Flex, Spacer, useColorModeValue, Heading, Skeleton, Select } from '@chakra-ui/react';
 import React, { useEffect, useState } from 'react';
 
 const CountryTable = ({ data, isLoading }) => {
@@ -13,6 +13,11 @@ const CountryTable = ({ data, isLoading }) => {
 
     const bg = useColorModeValue("white", "gray.800");
 
+    const [Data, setData] = useState('confirmed');
+    const CaseTypeSelector = (e) => {
+        setData(e.target.value);
+    };
+
     return (
         <Box
             rounded='xl'
@@ -20,13 +25,32 @@ const CountryTable = ({ data, isLoading }) => {
             p={3} bg={bg}
             my={6}
         >
-            <Heading
-                fontWeight={600}
-                as='h3'
-                size='md'
-                mb={3}>
-                Top Countries
+            <Box
+                display="flex"
+                flexDirection="row"
+                flexWrap="wrap">
+                <Heading
+                    flex="80%"
+                    fontWeight={600}
+                    as='h3'
+                    size='md'
+                    mb={3}>
+                    Top Countries
                 </Heading>
+                <Select
+                    onChange={CaseTypeSelector}
+                    flex="10%"
+                    size='sm'
+                    rounded='md'
+                    minW={110}
+                    mb={3}>
+                    <option value="confirmed">Confirmed</option>
+                    <option value="active">Active</option>
+                    <option value="recovered">Recovered</option>
+                    <option value="deaths">Deaths</option>
+                </Select>
+            </Box>
+
             <Box
                 display="flex"
                 flexDirection="column">
@@ -44,11 +68,11 @@ const CountryTable = ({ data, isLoading }) => {
                         {CountryData !== null && CountryData.filter((val) => {
                             return val.properties.flag !== '';
                         }).sort((a, b) => {
-                            return parseFloat(b.properties.confirmed.replace(/[^0-9-.]/g, '')) - parseFloat(a.properties.confirmed.replace(/[^0-9-.]/g, ''));
+                            return parseFloat(b.properties[Data].replace(/[^0-9-.]/g, '')) - parseFloat(a.properties[Data].replace(/[^0-9-.]/g, ''));
                         }).map((val, key) => {
                             return (
                                 <div key={key}>
-                                    <Flex flexDirection="row" alignItems="center" my={1} flexWrap="nowrap">
+                                    <Flex flexDirection="row" alignItems="center" my={2} flexWrap="nowrap">
                                         <img
                                             style={{ borderRadius: "3px" }}
                                             src={val.properties.flag}
@@ -56,7 +80,7 @@ const CountryTable = ({ data, isLoading }) => {
                                             height="20px" width="34px" />
                                         <Heading mx={3} size="xs" fontWeight={500}>{val.properties.ADMIN}</Heading>
                                         <Spacer />
-                                        <Heading size="xs" mx={1}>{val.properties.confirmed}</Heading>
+                                        <Heading size="xs" mx={1}>{val.properties[Data]}</Heading>
                                     </Flex>
                                     <hr />
                                 </div>
